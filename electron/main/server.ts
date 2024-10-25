a7e1d0f3d5fe55262a661a626a91d8aecfba259e
moluoxixi <10811655+moluoxixi@user.noreply.gitee.com>
2024-10-25 13:43:33 +0800

    修改

--- electron/main/server.ts
+++ electron/main/server.ts
@@ -2,24 +2,8 @@ import { join } from "path";
 import { fork, spawn, exec } from 'child_process';
 import { EOL } from "os";
 import Chalk from "chalk";
-import electron, { app } from 'electron';
+import  { app } from 'electron';
 
-function threadCommunication(child) {
-  // 监听来自子进程的消息
-  child.on('message', (args) => {
-    const { api, handleName, arg,id } = args;
-    let res;
-    // if (handleName) {
-    //   res = electron[api][handleName](arg);
-    // } else {
-    //   res = electron[api](arg);
-    // }
-    child.send({
-      res:1,
-      id
-    });
-  });
-}
 function sendMessageToRenderer(mainWindow,msg) {
   mainWindow?.webContents.send('send-for-main-propress', msg);
 }
@@ -31,7 +15,6 @@ function createServerHandle(serverPath, mainWindow, ...args) {
     stdio: ['ignore', 'pipe', 'pipe'], // 忽略输入，将标准输出和标准错误重定向到管道
     shell: true, // 在 Windows 上可能需要设置为 true
   });
-  threadCommunication(child);
   function stop() {
     if (child) {
       child.removeAllListeners('exit')
@@ -73,7 +56,6 @@ function createServer(mainWindow) {
     createServerHandle(serverPath, mainWindow, 'node ' + serverPath)
   } else {
     const serverPath = join(__dirname, '..', '..', '..', 'server')
-    console.log('serverPath', serverPath);
     createServerHandle(serverPath, mainWindow, 'npm', ['run', 'dev'])
   }
 
