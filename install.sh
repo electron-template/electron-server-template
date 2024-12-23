12b07a8ef80f14249121e8ce4411bbfcbac54c16
wl <1983531544@qq.com>
2024-12-23 22:57:28 +0800

    feat: 修改脚本

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ echo "$(tput setaf 4)正在初始化electron项目... $(tput setaf 3)"
 git checkout main && git pull
 pnpm install
 
+echo "$(tput setaf 4)正在初始化子模块中... $(tput setaf 3)"
+git submodule update
 
 echo "$(tput setaf 4)正在初始化renderer项目... $(tput setaf 3)"
 cd renderer
