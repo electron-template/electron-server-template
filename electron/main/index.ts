7c0c8afba583f4039d553a3d2ed6bb0d683ac8bf
moluoxixi <10811655+moluoxixi@user.noreply.gitee.com>
2024-12-26 19:36:27 +0800

    perf

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,37 +1,45 @@
-import {app, session} from 'electron';
-import os from "node:os";
-import createTray from "./tary";
+import { app, session } from 'electron';
+import os from 'node:os';
+import createTray from './tary';
 import createWindow from './mainWindow';
 import createRequest from './request';
-import createServer from './server'
-import Chalk from "chalk";
+import createServer from './server';
 
+
+let stopServerHandler = null;
 app.whenReady().then(async () => {
-    createTray();
-    createRequest();
+  createTray();
+  createRequest();
 
-    const mainWindow = await createWindow();
-    await createServer(mainWindow);
-    session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
-        callback({
-            responseHeaders: {
-                ...details.responseHeaders,
-                'Content-Security-Policy': ['script-src \'self\'']
-            }
-        })
-    })
-})
+  const mainWindow = await createWindow();
+  stopServerHandler = await createServer(mainWindow);
+  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+    callback({
+      responseHeaders: {
+        ...details.responseHeaders,
+        'Content-Security-Policy': ['script-src \'self\''],
+      },
+    });
+  });
+});
 
 // 关闭Windows 7的GPU加速
-if (os.release().startsWith('6.1')) app.disableHardwareAcceleration()
+if (os.release().startsWith('6.1')) app.disableHardwareAcceleration();
 
 // 设置Windows 10+通知的应用程序名称
-if (process.platform === 'win32') app.setAppUserModelId(app.getName())
+if (process.platform === 'win32') app.setAppUserModelId(app.getName());
 
 // 当窗口全部关闭时,退出electron应用,关闭进程
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-        process.exit()
-    }
+  if (process.platform !== 'darwin') {
+    stopServerHandler?.();
+    app.quit();
+    process.exit();
+  }
+});
+
+app.on('before-quit', () => {
+  stopServerHandler?.();
+  app.quit();
+  process.exit();
 });
--- electron/main/server.ts
+++ electron/main/server.ts
@@ -3,14 +3,15 @@ import { spawn } from 'child_process';
 import { EOL } from 'os';
 import Chalk from 'chalk';
 import { app } from 'electron';
-import { NestFactory } from '@nestjs/core'
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../../server/app.module';
+
 function sendMessageToRenderer(mainWindow, msg) {
   mainWindow?.webContents.send('send-for-main-propress', msg);
 }
 
 function createServerHandle(serverPath, mainWindow, ...args) {
-  const [command, arg = [],cwd] = args;
+  const [command, arg = [], cwd] = args;
   sendMessageToRenderer(mainWindow, `正在启动服务...,路径为${serverPath}`);
   const child = spawn(command, arg, {
     stdio: ['ignore', 'pipe', 'pipe'], // 忽略输入，将标准输出和标准错误重定向到管道
@@ -43,11 +44,11 @@ function createServerHandle(serverPath, mainWindow, ...args) {
   // 监听子进程的关闭事件
   child.on('close', (code) => {
     sendMessageToRenderer(mainWindow, '服务已关闭');
-    console.log(`child process exited with code ${code}`);
+    console.log(`child process close with code ${code}`);
     stop();
   });
   child.on('exit', (code) => {
-    sendMessageToRenderer(mainWindow, '服务已关闭');
+    sendMessageToRenderer(mainWindow, '服务已退出');
     console.log(`child process exited with code ${code}`);
     stop();
   });
@@ -61,6 +62,9 @@ async function createServer(mainWindow) {
 
     const app = await NestFactory.create(AppModule);
     await app.listen(process.env.PORT ?? 3000);
+    return async () => {
+      await app.close();
+    };
   }
 }
 
