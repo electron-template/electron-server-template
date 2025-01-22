6cc3664ef5db00a8ad0f2da6cc77d07e592920d6
wanglun <wrnm.@.com>
2025-01-22 17:56:32 +0800

    迁移github

--- installAll.sh
+++ installAll.sh
@@ -8,10 +8,6 @@ git pull
 echo "$(tput setaf 4)正在下载依赖... $(tput setaf 3)"
 pnpm install
 
-# 初始化子模块的配置文件
-echo "$(tput setaf 4)正在初始化子模块中... $(tput setaf 3)"
-git submodule init
-
 # 更新子模块到其远程跟踪分支的最新提交（使用 --rebase 可以避免合并提交，保持历史干净）
 # 注意：这里使用了 --rebase，但您也可以根据需要选择 --merge
 echo "$(tput setaf 4)正在更新子模块... $(tput setaf 3)"
