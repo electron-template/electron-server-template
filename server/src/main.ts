6d88a86ebce1139bf1f80c7926c8e6278338e9c1
moluoxixi <1983531544@qq.com>
2025-06-16 11:46:19 +0800

    feat: 修改

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,14 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // 全局管道 - 用于验证请求数据
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  // // 全局管道 - 用于验证请求数据
+  // app.useGlobalPipes(new ValidationPipe({ transform: true }));
   
   // 启用CORS
   app.enableCors();
   
-  // 应用前缀
-  app.setGlobalPrefix('api');
+  // // 应用前缀
+  // app.setGlobalPrefix('api');
   
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
---
+++ server/src/modules/modules.ts
@@ -0,0 +1,6 @@
+import { TestModule } from './test/test.module';
+
+const modules=[
+  TestModule
+]
+export default modules;
